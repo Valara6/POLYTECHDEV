eab7a67baacb069868e9d0f14448aaf848e79958
Валерий Денисов <valerad2212@mail.ru>
2024-09-21 16:50:14 +0300

    test commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", ip_address=request.remote_addr)
 
 if __name__ == "__main__":
     app.run()
