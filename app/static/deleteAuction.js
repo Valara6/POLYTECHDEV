f70910bc29a61d977eaf7d949e1631f36dc02dee
Valara6 <113579791+Valara6@users.noreply.github.com>
2024-12-07 11:17:26 +0300

    Update deleteAuction.js

--- app/static/deleteAuction.js
+++ app/static/deleteAuction.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let deleteAuctionModal = document.querySelector('#deleteAuction');
 
+    deleteAuctionModal.addEventListener('show.bs.modal', function(event) {
+        let button = event.relatedTarget; // Кнопка, вызвавшая модальное окно
+        let form = deleteAuctionModal.querySelector('.modal-form');
+        let auction = deleteAuctionModal.querySelector('.auction-title');
+
+        // Подставляем действие (action) формы
+        form.action = button.dataset.action;
+
+        // Получаем название аукциона из первой ячейки строки
+        auction.textContent = button.closest('tr').querySelector('td:first-child').textContent;
+    });
+});
